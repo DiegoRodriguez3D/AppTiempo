ba87afe142806f1285ab87f7fca40b9581f7a7f1
DiegoRodriguez3D <69866476+DiegoRodriguez3D@users.noreply.github.com>
2023-04-15 15:44:33 +0200

    Cambio UI MessageCard

--- app/src/main/java/com/diegorb/tiempo/presentation/components/MessageCard.kt
+++ app/src/main/java/com/diegorb/tiempo/presentation/components/MessageCard.kt
@@ -6,15 +6,19 @@ import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.diegorb.tiempo.presentation.ui.theme.DeepRed
 
 @Composable
 fun MessageCard(
@@ -35,49 +39,74 @@ fun MessageCard(
         shape = RoundedCornerShape(10.dp),
         elevation = 5.dp,
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+        ConstraintLayout(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(horizontal = 20.dp, vertical = 20.dp)
         ) {
 
-            Row(
+            val (iconID, titleID, msgID, divID) = createRefs()
+
+            Icon(
                 modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .size(40.dp),
-                    imageVector = icon,
-                    contentDescription = null,
-                    tint = iconColor
-                )
+                    .size(40.dp)
+                    .constrainAs(iconID) {
+                        end.linkTo(titleID.start)
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                    },
+                imageVector = icon,
+                contentDescription = null,
+                tint = iconColor
+            )
 
-                Spacer(modifier = Modifier.width(12.dp))
-
-                Text(
-                    text = title,
-                    textAlign = TextAlign.Center,
-                    fontSize = 30.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
+            Text(
+                modifier = Modifier
+                    .constrainAs(titleID) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                text = title,
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
 
             Divider(
-                modifier = Modifier.padding(horizontal = 40.dp, vertical = 5.dp),
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 5.dp)
+                    .constrainAs(divID) {
+                        top.linkTo(titleID.bottom)
+                    },
                 thickness = 2.dp,
                 color = Color.LightGray
             )
 
             Text(
+                modifier = Modifier
+                    .padding(top = 15.dp, bottom = 20.dp)
+                    .constrainAs(msgID) {
+                        top.linkTo(divID.bottom)
+                        bottom.linkTo(parent.bottom)
+                    },
                 text = msg,
                 color = textColor,
-                textAlign = TextAlign.Justify,
-                modifier = Modifier
-                    .padding(start = 20.dp, end = 20.dp, top = 5.dp, bottom = 20.dp)
+                textAlign = TextAlign.Start,
             )
         }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun previewMessage() {
+    val error =
+        "No se pudo recuperar la ubicación del dispositivo. Asegúrate de activar la ubicación y conceder los permisos necesarios."
+    MessageCard(
+        title = "Advertencia",
+        msg = error,
+        icon = Icons.Rounded.Warning,
+        iconColor = DeepRed
+    )
 }
