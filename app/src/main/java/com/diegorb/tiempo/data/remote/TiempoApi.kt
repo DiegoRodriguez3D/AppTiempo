b1b936575d133b0a96defe28b2174f6739e542da
DiegoRodriguez3D <69866476+DiegoRodriguez3D@users.noreply.github.com>
2023-04-16 01:19:31 +0200

    Fix zonas horarias

--- app/src/main/java/com/diegorb/tiempo/data/remote/TiempoApi.kt
+++ app/src/main/java/com/diegorb/tiempo/data/remote/TiempoApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.Query
 
 //Conecta con la api OpenMeteo para obtener los datos necesarios en un JSON
 interface TiempoApi {
-    @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl")
+    @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl&timezone=auto")
     suspend fun getTiempoData(
         @Query("latitude") lat: Double,
         @Query("longitude") long: Double
